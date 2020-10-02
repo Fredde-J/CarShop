4f0f11b76cde91531f3d674e977f01369dcc7211
Fredrik Jönsson <fredrikjay@gmail.com>
2020-10-02 11:08:11 +0200

    added startEngine method in car class and engineSound variable

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -6,12 +6,17 @@ public abstract class Car {
     public int numberOfDoors;
     public int numberOfSeats;
     public String fuelSource;
+    public String engineSound;
 
-    Car(int numberOfDoors, int numberOfSeats, int numberOfWheels, int horsePower , String fuelSource){
+    Car(int numberOfDoors, int numberOfSeats, int numberOfWheels, int horsePower , String fuelSource, String engineSound){
         this.numberOfDoors = numberOfDoors;
         this.numberOfSeats = numberOfSeats;
         this.numberOfWheels = numberOfWheels;
         this.horsePower=horsePower;
         this.fuelSource=fuelSource;
+        this.engineSound=engineSound;
+    }
+    public void startEngine(){
+        System.out.println("The car makes a "+engineSound+"when it starts and is ready to release "+horsePower+" hp");
     }
 }
