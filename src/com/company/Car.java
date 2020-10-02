fdf03316744842ca98851edaaf508de3120ddbac
Fredrik Jönsson <fredrikjay@gmail.com>
2020-10-02 10:55:12 +0200

    add variables and the constructor for Car Class

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -1,5 +1,17 @@
 package com.company;
 
 public abstract class Car {
-    
+    public int numberOfWheels;
+    public int horsePower;
+    public int numberOfDoors;
+    public int numberOfSeats;
+    public String fuelSource;
+
+    Car(int numberOfDoors, int numberOfSeats, int numberOfWheels, int horsePower , String fuelSource){
+        this.numberOfDoors = numberOfDoors;
+        this.numberOfSeats = numberOfSeats;
+        this.numberOfWheels = numberOfWheels;
+        this.horsePower=horsePower;
+        this.fuelSource=fuelSource;
+    }
 }
