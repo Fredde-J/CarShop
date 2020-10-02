2424565c06823419b0cf8f15d7dedb18ebdb2957
Fredrik Jönsson <fredrikjay@gmail.com>
2020-10-02 11:17:37 +0200

    Volkswagen now extends the Car class and added a constructor

--- src/com/company/Volkswagen.java
+++ src/com/company/Volkswagen.java
@@ -1,4 +1,13 @@
 package com.company;
 
-public class Volkswagen {
+public class Volkswagen extends Car {
+
+    Volkswagen(int numberOfDoors,
+               int numberOfSeats,
+               int numberOfWheels,
+               int horsePower,
+               String fuelSource,
+               String engineSound) {
+        super(numberOfDoors, numberOfSeats, numberOfWheels, horsePower, fuelSource, engineSound);
+    }
 }
