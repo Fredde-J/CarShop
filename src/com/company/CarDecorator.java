f1b2d644e5e2854f8806a58a434cf64ebd9777c9
Fredrik <Fredrikjay@gmail.com>
2020-10-03 13:20:07 +0200

    removed the constructor for CarDecorator, use basic constructor insead

--- src/com/company/CarDecorator.java
+++ src/com/company/CarDecorator.java
@@ -1,11 +1,7 @@
 package com.company;
 
 public class CarDecorator implements CarFunctions {
-    private CarFunctions carFunctions;
 
-    CarDecorator(CarFunctions carFunctions){
-        this.carFunctions = carFunctions;
-    }
     @Override
     public void startEngine() {
 
