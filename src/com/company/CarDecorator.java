222dc7a932f881131eb01cd56b7b76c01c4ce1b1
Fredrik <Fredrikjay@gmail.com>
2020-10-03 11:59:12 +0200

    Added a constructor for CarDecorator

--- src/com/company/CarDecorator.java
+++ src/com/company/CarDecorator.java
@@ -1,8 +1,13 @@
 package com.company;
 
 public class CarDecorator implements CarFunctions {
+    private CarFunctions carFunctions;
+
+    CarDecorator(CarFunctions carFunctions){
+        this.carFunctions = carFunctions;
+    }
     @Override
     public void startEngine() {
-        
+
     }
 }
