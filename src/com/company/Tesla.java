a5cce739c3e48e28982d5f876ad7ac2af237a04e
Fredrik <Fredrikjay@gmail.com>
2020-10-08 14:16:25 +0200

    Tesla now extends Car and implements is constructor and methods

--- src/com/company/Tesla.java
+++ src/com/company/Tesla.java
@@ -1,4 +1,22 @@
 package com.company;
 
-public class Tesla {
+public class Tesla extends Car {
+    public Tesla(int numberOfDoors, int numberOfSeats, int numberOfWheels, int horsePower, String fuelSource, String engineSound, String model, String color) {
+        super(numberOfDoors, numberOfSeats, numberOfWheels, horsePower, fuelSource, engineSound, model, color);
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("The Testa starts and make a"+ engineSound +" sound");
+    }
+
+    @Override
+    public void refuelTheCar() {
+
+    }
+
+    @Override
+    public void pedlalToTheMetal() {
+
+    }
 }
