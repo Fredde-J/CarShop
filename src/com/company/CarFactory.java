a4beafe31e513db34bfc73c29bab44c60237afd3
Fredrik <Fredrikjay@gmail.com>
2020-10-08 12:11:57 +0200

    Added method changeColor in CarFactory
    ChangeColor use setColorRed in RedCarDecorator Class and return a car that now has the color of red

--- src/com/company/CarFactory.java
+++ src/com/company/CarFactory.java
@@ -5,9 +5,16 @@ public class CarFactory {
         Car car = null;
         switch (Brand){
             case "Volkswagen":
-                car = new Volkswagen(2,5,4,25,"gasolin","BROOM POP PANG BROM","beetle");
+                car = new Volkswagen(2,5,4,25,"Gasoline","BROOM POP PANG BROM","Beetle","Black");
 
         }
      return car;
+    }
+    public static Car changeColor(String color, Car car){
+    switch (color){
+        case "Red":
+         car = new RedCarDecorator(car).setColorRed(car);
+    }
+    return car;
     }
 }
