89fa058dfe1d67d4b1791c195ef7c8f719aea3e6
Fredrik <Fredrikjay@gmail.com>
2020-10-08 14:41:59 +0200

    Bmw class now extends Car and implements is constructor and methods

--- src/com/company/Bmw.java
+++ src/com/company/Bmw.java
@@ -1,4 +1,22 @@
 package com.company;
 
-public class Bmw {
+public class Bmw extends Car {
+    public Bmw(int numberOfDoors, int numberOfSeats, int numberOfWheels, int horsePower, String fuelSource, String engineSound, String model, String color) {
+        super(numberOfDoors, numberOfSeats, numberOfWheels, horsePower, fuelSource, engineSound, model, color);
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("The "+model+" starts and make a "+ engineSound );
+    }
+
+    @Override
+    public void refuelTheCar() {
+
+    }
+
+    @Override
+    public void pedlalToTheMetal() {
+
+    }
 }
