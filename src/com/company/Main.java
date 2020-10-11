ba7ce60dabd1417f2eb3c69085791a1a04d8cc13
Fredrik <Fredrikjay@gmail.com>
2020-10-11 15:18:57 +0200

    main calls getInstance method instead of the constructor for CarMenu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	CarMenu carMenu = new CarMenu();
-	carMenu.mainMenu();
+	CarMenu carMenu = CarMenu.getInstance();
+	     carMenu.mainMenu();
     }
 }
